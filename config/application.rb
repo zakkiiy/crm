6e88b271be8a821d0356ac7b1a90d8c30c519348
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-03-19 01:38:26 +0900

    Timezoneの設定、日本語化設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Baukis
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # timezoneの設定
+    config.time_zone = "Tokyo"
+    config.i18n.load_path +=
+      Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
